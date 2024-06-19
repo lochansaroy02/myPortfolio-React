9c38bb16139c907f0cf771164f4bb2050da3959e
lochansaroy02 <lochan.kumar.fit.csebct20@teamfuture.in>
2024-06-19 19:51:56 +0530

    update about

--- src/components/About.js
+++ src/components/About.js
@@ -84,6 +84,9 @@ const Person = () => {
   return (
     <div className=' w-screen h-fit   items-center  '>
       <svg _ngcontent-itj-c18="" aria-hidden="true" width="100%" height="8" fill="none" xmlns="http://www.w3.org/2000/svg"><pattern _ngcontent-itj-c18="" id="a" width="91" height="8" patternUnits="userSpaceOnUse"><g _ngcontent-itj-c18="" clip-path="url(#clip0_2426_11367)"><path _ngcontent-itj-c18="" d="M114 4c-5.067 4.667-10.133 4.667-15.2 0S88.667-.667 83.6 4 73.467 8.667 68.4 4 58.267-.667 53.2 4 43.067 8.667 38 4 27.867-.667 22.8 4 12.667 8.667 7.6 4-2.533-.667-7.6 4s-10.133 4.667-15.2 0S-32.933-.667-38 4s-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0-10.133-4.667-15.2 0-10.133 4.667-15.2 0" stroke="#E1E3E1" stroke-linecap="square"></path></g></pattern><rect _ngcontent-itj-c18="" width="100%" height="100%" fill="url(#a)" opacity="0.3"></rect></svg>
+    
+    
+    
       <div className='  rounded-lg  mt-8 text-neutral-100 h-screen   p-8 flex border-gray'>
         <div className='w-1/2   h-2/3 py-4 flex  flex-col justify-between  '>
 
@@ -123,7 +126,7 @@ const Person = () => {
           <h1 className='text-6xl'>
             Skills
           </h1>
-          <ul className=' grid grid-cols-4   gap-3 py-8  '>
+          <ul className=' grid grid-cols-3   gap-3 py-8  '>
             {skills.map((skill) => {
               return (
                 <div className='bg-neutral-700 text-white   rounded-lg py-4'>
