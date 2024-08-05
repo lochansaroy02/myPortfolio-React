ef0b0cfdcd598f8a046891212ddd5b52d60be5e3
lochansaroy02 <lochan.kumar.fit.csebct20@teamfuture.in>
2024-08-05 16:11:41 +0530

    Added Skills

--- src/components/About.js
+++ src/components/About.js
@@ -8,44 +8,52 @@ const Person = () => {
     {
       name: 'HTML',
       icon: <i class="devicon-html5-plain-wordmark colored"></i>
-
     },
     {
       name: 'CSS',
       icon: <i class="devicon-css3-plain-wordmark colored"></i>
-
     },
     {
       name: 'JavaScript',
       icon: <i class="devicon-javascript-plain colored"></i>
-
     },
     {
       name: 'React',
       icon:
         <i class="devicon-react-original colored"></i>
-
     },
 
+    {
+      name: 'Node js',
+      icon:
+      <i class="devicon-nodejs-plain-wordmark colored"></i>
+          
+    },
+    {
+      name: 'mongodb',
+      icon:
+
+        
+      <i class="devicon-mongodb-plain colored"></i>
+    },
+    {
+      name: 'express',
+      icon:
+      <i class="devicon-express-original"></i>
+    },
 
     {
       name: 'Java',
       icon:
         <i class="devicon-java-plain colored"></i>
-
     },
     {
       name: 'Git',
       icon:
 
         <i class="devicon-git-plain colored"></i>
-
-
-    },
-
-    
-
-
+    }
+   
   ];
 
   let social = [
@@ -77,81 +85,80 @@ const Person = () => {
       name: "Facebook",
       iconLink: "https://facebook.com",
       link: 'https://www.linkedin.com/in/lochankumar47/'
-
     },
 
 
   ]
   return (
-<>
+    <>
 
-    <div className=' w-screen   items-center  '>
-    
-    
-    
-      <div className='  dark:text-neutral-100 dark:bg-neutral-800 bg-neutral-200  pb-12   md:p-8 md:flex border-gray'>
-        <div className='md:w-1/2   md:h-2/3 py-4 flex  flex-col justify-between  '>
+      <div className=' w-screen   items-center  '>
 
-          <div className=' ml-4 p-4'>
-            <h1 className=' font-poppins text-4xl md:text-6xl'>
 
-              About
-            </h1>
 
-            <p className='text-wrap md:text-xl mr-8  text-justify mt-8'>
-            Blending creativity with technical expertise in HTML, CSS, JavaScript, React, and Node.js. I develop robust, scalable web applications that captivate users. Your project deserves the best <br/> – let’s work together!
-            </p>
+        <div className='  dark:text-neutral-100 dark:bg-neutral-800 bg-neutral-200  pb-12   md:p-8 md:flex border-gray'>
+          <div className='md:w-1/2   md:h-2/3 py-4 flex  flex-col justify-between  '>
+
+            <div className=' ml-4 p-4'>
+              <h1 className=' font-poppins text-4xl md:text-6xl'>
+
+                About
+              </h1>
+
+              <p className='text-wrap md:text-xl mr-8  text-justify mt-8'>
+                Blending creativity with technical expertise in HTML, CSS, JavaScript, React, and Node.js. I develop robust, scalable web applications that captivate users. Your project deserves the best <br /> – let’s work together!
+              </p>
+            </div>
+
+            <div className=' mt-4 md:w-3/4 m-8 flex dark:bg-neutral-800 bg-zinc-300  ml-6 border border-neutral-800 dark:border-white rounded-2xl p-4 justify-around '>
+              {
+                social.map((item) => {
+                  return (
+                    <div className=' '>
+
+                      <SocialIcon url={item.iconLink} />
+                      <h1 className='md:text-sm  text-xs  mt-2'>
+                        {item.name}
+                      </h1>
+                    </div>
+                  )
+                })
+              }
+
+
+            </div>
+
+
+
           </div>
-
-          <div className=' mt-4 md:w-3/4 m-8 flex dark:bg-neutral-800 bg-zinc-300  ml-6 border border-neutral-800 dark:border-white rounded-2xl p-4 justify-around '>
-            {
-              social.map((item) => {
+          <div className=' dark:bg-neutral-800 bg-zinc-300      rounded-2xl border m-8  border-neutral-700 dark:border-white md:w-[40%] p-4'>
+            <h1 className='text-4xl md:text-6xl'>
+              Skills
+            </h1>
+            <ul className=' grid grid-cols-3   gap-3 py-8  '>
+              {skills.map((skill) => {
                 return (
-                  <div className=' '>
+                  <div className='dark:bg-neutral-700 bg-zinc-200 border-neutral-600 dark:text-white   rounded-xl py-4'>
+                    <li className='mx-2 text-md flex flex-col-reverse items-center'>
+                      <h1 className='md:text-xl '>
 
-                  <SocialIcon url={item.iconLink} />
-                  <h1 className='md:text-sm  text-xs  mt-2'>
-                    {item.name}
-                  </h1>
+                        {skill.name}
+                      </h1>
+                      <h1 className='md:text-6xl text-4xl  mb-2'>
+
+                        {skill.icon}
+                      </h1>
+                    </li>
                   </div>
                 )
-              })
-            }
+              })}
 
 
+            </ul>
           </div>
-
-
-
-        </div>
-        <div className=' dark:bg-neutral-800 bg-zinc-300      rounded-2xl border m-8  border-neutral-700 dark:border-white md:w-[40%] p-4'>
-          <h1 className='text-4xl md:text-6xl'>
-            Skills
-          </h1>
-          <ul className=' grid grid-cols-3   gap-3 py-8  '>
-            {skills.map((skill) => {
-              return (
-                <div className='dark:bg-neutral-700 bg-zinc-200 border-neutral-600 dark:text-white   rounded-xl py-4'>
-                  <li className='mx-2 text-md flex flex-col-reverse items-center'>
-                    <h1 className='md:text-xl '>
-
-                      {skill.name}
-                    </h1>
-                    <h1 className='md:text-6xl text-4xl  mb-2'>
-
-                      {skill.icon}
-                    </h1>
-                  </li>
-                </div>
-              )
-            })}
-
-
-          </ul>
         </div>
       </div>
-    </div>
-   </>
+    </>
   )
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,31 +1,37 @@
 import React from 'react';
 import { TypeAnimation } from 'react-type-animation';
-import img from '../utils/img/profile.png'
+import img from '../utils/img/profile.png';
 
 
 
 const Home = () => {
 
     const handleCLick = () => {
-        
-    const pdfPath = `${process.env.PUBLIC_URL}/sample.pdf`;
-    const link = document.createElement('a');
-    link.href = pdfPath;
-    link.download = 'sample.pdf';
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
+
+        const pdfPath = `${process.env.PUBLIC_URL}/sample.pdf`;
+        const link = document.createElement('a');
+        link.href = pdfPath;
+        link.download = 'sample.pdf';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
     }
 
-    
+
 
 
     let image = 'https://avatars.githubusercontent.com/u/104918582?s=400&u=69829e4089cdc26774e0154b400d60c6871f2a73&v=4'
     return (
-        <div className='  flex  items-center     justify-center  w-screen h-screen md:p-4  bg-neutral-300 dark:bg-neutral-900' >
+
+        <div className='  flex   items-center     justify-center  w-screen h-screen md:p-4  bg-neutral-300 dark:bg-neutral-900' >
+            {/* <Shape width={200} height={200} fillColor="red" />  */}
             <div className='flex w-[90%]  md:p-8  h-fit  flex-col  md:flex-row-reverse  rounded-2xl text-white'>
-                <div className=' border-w h-full   flex justify-center items-center  w-fit rounded-xl mr-2'>
-                    <img className='md:h-[80%] md:w-[80%]   bg-blue-600 dark:bg-blue-400 rounded-3xl ' src={img} alt="" /> 
+                <div className=' border-w h-full   flex justify-center items-center  w-fit rounded-full mr-2'>
+                    <img className='md:h-[70%] md:w-[70%]   bg-blue-600 dark:bg-blue-400 rounded-full ' src={img} alt="" />
+
+
+
+
                 </div>
 
 
@@ -59,7 +65,7 @@ const Home = () => {
 
                         <div className='md:mt-16 mt-8'>
 
-                        <button className=' bg-blue-600  px-4 py-2 rounded-lg' onClick={handleCLick} > Hire me!  </button>
+                            <button className=' bg-blue-600  px-4 py-2 rounded-lg' onClick={handleCLick} > Hire me!  </button>
                         </div>
 
 
